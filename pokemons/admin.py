5bffe68359ec1e3494f4e6fb29307c4c1d18945e
Fabien <fhannon60@gmail.com>
2021-03-11 19:41:02 +0100

    models added to admin site

--- pokemons/admin.py
+++ pokemons/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Pokemon
+from .models import Pokemon, Stat,Type
 
 # Register your models here.
+admin.site.register(Pokemon)
+admin.site.register(Stat)
+admin.site.register(Type)
